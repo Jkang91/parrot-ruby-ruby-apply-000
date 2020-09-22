269a003c0266510755ba2aa8d870d1ee2808a868
June Young Kang <jkang34@gmail.com>
2020-09-22 07:10:41 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,6 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(name = "Squawk!", phrase = "Pretty bird!")
+  phrase = parrot("Pretty bird!")
+  puts "#{name}"
+  "Squawk!"
+end
+
